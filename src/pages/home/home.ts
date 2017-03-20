3103e74011f8ca2eebf4f334b2ac782a08437dbb
Yuriy Pigovsky <pigovsky@gmail.com>
2017-03-20 10:41:12 +0200

    add todo home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,8 +8,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  items = [
+     { 
+        title: "Лекція з КППЗ"
+     }, 
+     { 
+        title: "Практична з КППЗ"
+     }, 
+  ]
+
   constructor(public navCtrl: NavController) {
     
   }
 
+
+  addItem() {
+    console.log("addItem");
+    this.items.push(
+		{ 
+        title: "beliberda"
+      } 
+    )
+  }
+
+  delete(item) {
+    console.log("delete item " + item.title);
+    this.items.splice(this.items.indexOf(item), 1);
+  }
 }
+
+

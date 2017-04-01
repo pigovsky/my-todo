cd78b27d2f844d17d148ac4e23e4733d7627f5b0
ivanyavornitskiy <ivanyavornitskiy@gmail.com>
2017-04-02 01:55:30 +0300

    Page for add items is done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/add-item/add-item';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+	AddItemPage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+	IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+	AddItemPage
   ],
   providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
 })

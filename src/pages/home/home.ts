c79b6b43f6f0f1475b2bea8518e511a2a1895ea3
nico <litvinchuk@lightsoft.ru>
2017-05-15 09:43:57 +0300

    розширення до використування API

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,44 +1,31 @@
-import { Component } from '@angular/core';
-
-import { NavController, ModalController } from 'ionic-angular';
-import { AddItemPage } from '../add-item/add-item'
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Http} from '@angular/http';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
+
 export class HomePage {
-  public items = [];
+    public items = []
 
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
+    constructor(public navCtrl: NavController, public http: Http) {
+        this.http.get('/api/values').map(res => res.json()).subscribe(data => {
+            this.items = data;
+        });
+    }
 
-  }
-
-
-  addItem() {
-    console.log("addItem");
-
-      let addModal = this.modalCtrl.create(AddItemPage);
-
-      addModal.onDidDismiss((item) => {
-
-          if(item){
-              this.saveItem(item);
-          }
-
-      });
-
-      addModal.present();
-  }
-
-    saveItem(item){
-        this.items.push(item);
+    addItem() {
+        this.items.push(
+            {
+                title: "beliberda"
+            }
+        )
     }
 
     delete(item) {
-    console.log("delete item " + item.title);
-    this.items.splice(this.items.indexOf(item), 1);
-  }
+        console.log("delete item " + item.title);
+        this.items.splice(this.items.indexOf(item), 1);
+    }
 }
-
-

22ccbe6ddf9ec7c910a7cd66944637f5278af278
PycJIaHK0 <Dubchakruslan@gmail.com>
2017-05-17 20:49:45 +0300

    Changes v.1.1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Http} from '@angular/http';
 
-import { NavController } from 'ionic-angular';
+import {NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -8,58 +9,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  items = [
-     { 
-        title: "Лекція з КППЗ"
-     }, 
-     { 
-        title: "Практична з КППЗ"
-     }, 
-  ]
+  items: any;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController, public http: Http) {
+    this.http.get('/api/values').map(function (res) {
+      return res.json();
+    }).subscribe(data => {
+      this.items = data;
+    });
   }
 
 
   addItem() {
-    console.log("addItem");
     this.items.push(
-		{ 
-		
-		
-        title: "Нове завдання"
-		
-      } 
+      {
+        title: "beliberda"
+      }
     )
-	
-	alert('Нове завдання створено успішно!');
   }
 
   delete(item) {
-    console.log("delete item " + item.title);
     this.items.splice(this.items.indexOf(item), 1);
   }
-
-  
-  
-  
-  
-  
-  
-  hreff(){
-	  
-	  window.open("https://ionicframework.com/");
-  }
-  
-  
-  
-  
-  
-  
-  
-  }
-
-
-
-
+}

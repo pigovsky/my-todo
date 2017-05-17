b13dc721288716766d5cf7913268ca34ec0c4a34
tolyan---tolyan <tolyan---tolyan@ukr.net>
2017-05-17 21:30:39 +0300

    виведення списку

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,40 +1,31 @@
-import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {Http} from '@angular/http';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
+
 export class HomePage {
+    public items = []
 
-  items = [
-     { 
-        title: "Лекція з КППЗ"
-     }, 
-     { 
-        title: "Практична з КППЗ"
-     }, 
-  ]
+    constructor(public navCtrl: NavController, public http: Http) {
+        this.http.get('/api/values').map(res => res.json()).subscribe(data => {
+            this.items = data;
+        });
+    }
 
-  constructor(public navCtrl: NavController) {
-    
-  }
+    addItem() {
+        this.items.push(
+            {
+                title: "beliberda"
+            }
+        )
+    }
 
-
-  addItem() {
-    console.log("addItem");
-    this.items.push(
-		{ 
-        title: "beliberda"
-      } 
-    )
-  }
-
-  delete(item) {
-    console.log("delete item " + item.title);
-    this.items.splice(this.items.indexOf(item), 1);
-  }
+    delete(item) {
+        console.log("delete item " + item.title);
+        this.items.splice(this.items.indexOf(item), 1);
+    }
 }
-
-

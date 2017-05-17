b23d979b3acd38044400114b90b5b9e682bf40f0
Yura Pasternak <pasternak2048@gmail.com>
2017-05-17 20:15:54 +0300

    Add Changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,47 +1,37 @@
 import { Component } from '@angular/core';
-
-import { NavController } from 'ionic-angular';
-
-@Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
-})
-export class HomePage {
-
-  items = [
-     { 
-        title: "Лекція з КППЗ"
-     }, 
-     { 
-        title: "Практична з КППЗ"
-     }, 
-  ]
-
-  constructor(public navCtrl: NavController) {
-    
-  }
-
-
-  addItem() {
-    console.log("addItem");
-    this.items.push(
-		{ 
-        title: "Нове завдання:"
-      } 
-    )
-  }
-
-  delete(item) {
-    console.log("delete item " + item.title);
-    this.items.splice(this.items.indexOf(item), 1);
-  }
-
-
-renameItem(){ 
-console.log("rename item"); 
-this.items.push( 
-{ 
-title:prompt("Введіть завдання","") 
-} 
-) 
-}}
+ +
+ +import { NavController, ModalController } from 'ionic-angular';
+ +import { AddItemPage } from '../add-item/add-item'
+ +import { Http } from '@angular/http';
+ +
+ +@Component({
+ +  selector: 'page-home',
+ +  templateUrl: 'home.html'
+ +})
+ +
+ +export class HomePage {
+ +  public items = []
+ +
+ +  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public http: Http) {
+ +    this.http.get('/api/values').map(res => res.json()).subscribe(data => {
+ +      this.items = data;
+ +    });
+ +  }
+ +
+ +  addItem() {
+ +    let addModal = this.modalCtrl.create(AddItemPage);
+ +    addModal.onDidDismiss((item) => {
+ +      if (item) { this.saveItem(item); }
+ +    });
+ +    addModal.present();
+ +  }
+ +
+ +  saveItem(item) {
+ +    this.items.push(item);
+ +  }
+ +
+ +  delete(item) {
+ +    console.log("delete item " + item.title);
+ +    this.items.splice(this.items.indexOf(item), 1);
+ +  }
+ +}

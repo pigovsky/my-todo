9d13d5467a1f642d797032ccc116bdc4aab9d010
DarmorostV <darmorost.ivan@gmail.com>
2017-06-24 00:49:24 +0300

    Update home.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,33 +1,49 @@
 import { Component } from '@angular/core';
+import { ModalController, NavController } from 'ionic-angular';
 import { AddItemPage } from '../pages/item add';
   		  
-  @Component({		  @Component({
-    selector: 'page-home',		    selector: 'page-home',
-    templateUrl: 'home.html'		    templateUrl: 'home.html'
-  })		  })
-  export class HomePage {		  export class HomePage {
-  		  
+ @Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
 
-             this.saveItem(item);
-           }
-  
-     });
-  
-     addModal.present();
-  
-    }		    }
-  		  
-    delete(item) {		    delete(item) {
+  items = [];
 
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController) {
  
-   saveItem(item){
-     this.items.push(item);
-   }
+  }
  
-   viewItem(item){
-   this.navCtrl.push(ItemDetailPage, {
-       item: item
-     });
-   }
-  }		  }
-  		  
+  ionViewDidLoad(){
+ 
+  }
+  addItem(){
+ 
+    let addModal = this.modalCtrl.create(AddItemPage);
+ 
+    addModal.onDidDismiss((item) => {
+ 
+          if(item){
+            this.saveItem(item);
+          }
+ 
+    });
+ 
+    addModal.present();
+ 
+  }
+
+  delete(item) {
+    this.items.splice(this.items.indexOf(item), 1);
+  }
+
+  saveItem(item){
+    this.items.push(item);
+  }
+
+  viewItem(item){
+  this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
+  }
+}

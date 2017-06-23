0961efeb55bda31161b1f013174a0947f303cd87
IvanYavornitskiy <IvanYavornitskiy@gmail.com>
2017-06-24 02:34:40 +0300

    Add files via upload

---
+++ src/providers/local-task-service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+ import {TaskDaoImpl} from "../../models/TaskDaoImpl";
+ import {Task} from "../../models/Task";
+ import {TaskService} from "../TaskService";
+ import {Observable} from "rxjs/Observable";
+ 
+ /*
+ Generated class for the LocalTaskServiceProvider provider.
+ See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+ for more info on providers and Angular 2 DI.
+*/
+ @Injectable()
+ export class LocalTaskServiceProvider implements TaskService {
+ 
+ constructor(public http: Http, public taskDaoImpl:TaskDaoImpl) {
+ }
+ 
+ all(): Observable<Array<Task>> {
+   return this.taskDaoImpl.all();
+ }
+ 
+ add(task: Task): Observable<Number> {
+   return  Observable.of(this.taskDaoImpl.add(task));
+ }
+ 
+ delete(task: Task): Observable<Number> {
+   return Observable.of(this.taskDaoImpl.delete(task));
+ }
+ 
+ deleteAll(){
+   this.taskDaoImpl.deleteAll();
+ }
+ 
+ }
---
+++ src/providers/task_service.ts
@@ -0,0 +1,88 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+import {TaskService} from "../TaskService";
+import {Observable} from "rxjs/Observable";
+import {Task} from "../../models/Task";
+import {RemoteTaskService} from "../RemoteTaskService";
+import {LocalTaskServiceProvider} from "../local-task-service";
+
+/*
+  Generated class for the TaskServiceProvider provider.
+  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
+  for more info on providers and Angular 2 DI.
+*/
+@Injectable()
+export class TaskServiceProvider implements TaskService {
+
+  public data: any;
+  public offline;
+  public online;
+  public taskService: TaskService;
+  public onlineState=false;
+
+  constructor(public remoteTaskService: RemoteTaskService, public localTaskService: LocalTaskServiceProvider, public http: Http) {
+    console.log('Hello TaskServiceProvider Provider');
+    this.offline = Observable.fromEvent(window, "offline");
+    this.online = Observable.fromEvent(window, "online");
+    this.taskService = remoteTaskService;
+    this.online.subscribe(() => {
+      this.taskService = remoteTaskService;
+      console.log("online!");
+      this.onlineState = true;
+      this.sync();
+    });
+    this.offline.subscribe(() => {
+      this.taskService = localTaskService;
+      this.onlineState = false;
+      console.log("offline!");
+    });
+  }
+
+  all(): Observable<Array<Task>> {
+    if (this.onlineState){
+      this.sync()
+    }
+    return this.taskService.all();
+  }
+
+  add(task: Task): Observable<Number> {
+    let res= this.taskService.add(task);
+    if (this.onlineState){
+      this.sync()
+    }
+    return res;
+  }
+
+  delete(task: Task): Observable<Number> {
+    let res=  this.taskService.delete(task);
+    if (this.onlineState){
+      this.sync()
+    }
+    return res;
+  }
+
+
+  sync() {
+    let localTasks ;
+    this.localTaskService.all().subscribe(data => {
+      localTasks = data;
+    });
+    let remoteTasks ;
+    this.remoteTaskService.all().subscribe(data => {
+      remoteTasks = data;
+    });
+
+    let mergedTasks = [new Set(remoteTasks.concat(localTasks))];
+
+    for(var task in mergedTasks){
+      if(!remoteTasks.includes(task)){
+        this.remoteTaskService.add(JSON.parse(task));
+      }
+      if(!localTasks.includes(task)){
+        this.localTaskService.add(JSON.parse(task));
+      }
+    }
+
+  }
+}

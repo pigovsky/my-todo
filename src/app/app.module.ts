5f13a8b9224ab72191538dc005560400bbce2d1d
DarmorostV <darmorost.ivan@gmail.com>
2017-06-24 01:00:44 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,27 @@
- import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
- import { MyApp } from './app.component';		  import { MyApp } from './app.component';
- import { HomePage } from '../pages/home/home';		  import { HomePage } from '../pages/home/home';
- import { AddItemPage } from '../pages/add-item/add-item';
- import { ItemDetailPage } from '../pages/item-detail/item-detail';
-  
-  @NgModule({		  @NgModule({
-    declarations: [		    declarations: [
-      MyApp,		      MyApp,
-     HomePage		 +    HomePage,
-     AddItemPage,
-     ItemDetailPage
-    ],		    ],
-    imports: [		    imports: [
-      IonicModule.forRoot(MyApp)		      IonicModule.forRoot(MyApp)
-    ],		    ],
-    bootstrap: [IonicApp],		    bootstrap: [IonicApp],
-    entryComponents: [		    entryComponents: [
-      MyApp,		      MyApp,
-     HomePage		 +    HomePage,
-     AddItemPage,
-     ItemDetailPage
-    ],		    ],
-    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]		    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
-  })		  })
- -export class AppModule {}		 +export class AppModule {}
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { AddItemPage } from '../pages/item-add';
+import { ItemDetailPage } from '../pages/item-detail/item-detail';
+ 
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AddItemPage,
+    ItemDetailPage
+  ],
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+})
+export class AppModule {}

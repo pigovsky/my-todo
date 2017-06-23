573d6c3a26f760a62e1a9287cc82304dbbbea3f4
DarmorostV <darmorost.ivan@gmail.com>
2017-06-24 00:34:40 +0300

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,26 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-
-@NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
-})
-export class AppModule {}
+ import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+ import { MyApp } from './app.component';		  import { MyApp } from './app.component';
+ import { HomePage } from '../pages/home/home';		  import { HomePage } from '../pages/home/home';
+ import { AddItemPage } from '../pages/add-item/add-item';
+ import { ItemDetailPage } from '../pages/item-detail/item-detail';
+  
+  @NgModule({		  @NgModule({
+    declarations: [		    declarations: [
+      MyApp,		      MyApp,
+     HomePage		 +    HomePage,
+     AddItemPage,
+     ItemDetailPage
+    ],		    ],
+    imports: [		    imports: [
+      IonicModule.forRoot(MyApp)		      IonicModule.forRoot(MyApp)
+    ],		    ],
+    bootstrap: [IonicApp],		    bootstrap: [IonicApp],
+    entryComponents: [		    entryComponents: [
+      MyApp,		      MyApp,
+     HomePage		 +    HomePage,
+     AddItemPage,
+     ItemDetailPage
+    ],		    ],
+    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]		    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+  })		  })
+ -export class AppModule {}		 +export class AppModule {}

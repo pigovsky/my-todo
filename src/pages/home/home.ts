b95ec8b59e768eb1ae264f30ad8fde9deaa702ba
PycJIaHK0 <dubchakruslan@gmail.com>
2017-06-24 10:53:47 +0300

    Add files via upload

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,34 +1,69 @@
-import {Component} from '@angular/core';
-import {Http} from '@angular/http';
+import { Component } from '@angular/core';
+import {AlertController, NavController} from 'ionic-angular';
+import {RemoteTaskService} from '../../providers/RemoteTaskService';
+import {AddPage} from "../add/add";
+import {EditPage} from "../edit/edit";
+import {Observable} from "rxjs/Observable";
+import {TaskServiceProvider} from "../../providers/task-service/task-service";
+import {Task} from "../../models/Task";
 
-import {NavController} from 'ionic-angular';
+
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+    providers: [TaskServiceProvider]
 })
 export class HomePage {
 
-  items: any;
+    public items: Array<string>;
+    public data: Array<Task>;
 
-  constructor(public navCtrl: NavController, public http: Http) {
-    this.http.get('/api/values').map(function (res) {
-      return res.json();
-    }).subscribe(data => {
-      this.items = data;
-    });
-  }
+    constructor(public taskService: TaskServiceProvider, private nav: NavController, private alertCtrl: AlertController) {
+    }
+
+    loadData(){
+        this.taskService.all().subscribe(
+            res => {
+                this.data=res;
+                this.getDataForView();
+            }
+        );
+    }
+
+    ionViewDidEnter() {
+        this.loadData();
+    }
+
+    private getDataForView() {
+        this.items = [];
+        for (var i = 0; i < this.data.length; i++) {
+            this.items.push(JSON.stringify(this.data[i]));
+        }
+    }
+ 
+    addItem() {
+        this.nav.push(AddPage);
+    }
+
+    delete(item: string) {
+        // console.log(JSON.parse(index).Id);
+        this.taskService.delete(JSON.parse(item).Id);
+        // this.items.splice(index, 1);
+        // localStorage.setItem("todos", JSON.stringify(this.items));
+    }
+
+    update(item: string) {
+        // console.log(JSON.parse(index).Id);
+        this.nav.push(EditPage, {
+            param1: JSON.parse(item)
+        })
+        // this.items.splice(index, 1);
+        // localStorage.setItem("todos", JSON.stringify(this.items));
+    }
 
 
-  addItem() {
-    this.items.push(
-      {
-        title: "beliberda"
-      }
-    )
-  }
 
-  delete(item) {
-    this.items.splice(this.items.indexOf(item), 1);
-  }
 }
+
+
+

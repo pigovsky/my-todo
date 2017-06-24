e76cf2014f289a0753eea3de19428cff10fa0b0b
tymchyshyn <volodymyr.kilchitsky@magnis.com.ua>
2017-06-24 09:52:21 +0300

    tymchyshyn

---
+++ src/models/SyncStatus.ts
@@ -0,0 +1,8 @@
+/**
+ * Created by U on 23.06.2017.
+ */
+export enum SyncStatus {
+    Synced,
+    New,
+    Removed
+}
---
+++ src/providers/ITaskService.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by U on 24.06.2017.
+ */
+import {Observable} from 'rxjs/Rx';
+import {Task} from "../models/Task";
+
+export interface ITaskService {
+all(): Observable<Array<Task>>
+add(task:Task): Observable<Number>
+delete(task:Task): Observable<Number>
+}

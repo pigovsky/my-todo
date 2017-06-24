3e0e83c0cdebc15730822abfb6c66a43d0949852
oksik01 <oksik1995@gmail.com>
2017-06-24 10:15:20 +0300

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,34 @@
-import { NgModule, ErrorHandler } from '@angular/core';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import {NgModule, ErrorHandler} from '@angular/core';
+import {IonicApp, IonicModule, IonicErrorHandler} from 'ionic-angular';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {AddPage} from "../pages/add/add";
+import {EditPage} from "../pages/edit/edit";
+import { LocalTaskServiceProvider } from '../providers/local-task-service/local-task-service';
+import { TaskServiceProvider } from '../providers/task-service/task-service';
+import {RemoteTaskService} from "../providers/RemoteTaskService";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    IonicModule.forRoot(MyApp)
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}]
+    declarations: [
+        MyApp,
+        HomePage,
+        AddPage,
+        EditPage,
+    ],
+    imports: [
+        IonicModule.forRoot(MyApp)
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        AddPage,
+        EditPage,
+    ],
+    providers: [{provide: ErrorHandler, useClass: IonicErrorHandler},
+    LocalTaskServiceProvider,
+    TaskServiceProvider,
+    RemoteTaskService]
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,62 +1,69 @@
 import { Component } from '@angular/core';
-import { ModalController, NavController } from 'ionic-angular';
-import { AddItemPage } from '../add-item/add-item'
-import { ItemDetailPage } from '../item-detail/item-detail';
-import {Http} from '@angular/http';
+import {AlertController, NavController} from 'ionic-angular';
+import {RemoteTaskService} from '../../providers/RemoteTaskService';
+import {AddPage} from "../add/add";
+import {EditPage} from "../edit/edit";
+import {Observable} from "rxjs/Observable";
+import {TaskServiceProvider} from "../../providers/task-service/task-service";
+import {Task} from "../../models/Task";
+
 
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+    providers: [TaskServiceProvider]
 })
 export class HomePage {
 
-public items = []
+    public items: Array<string>;
+    public data: Array<Task>;
+
+    constructor(public taskService: TaskServiceProvider, private nav: NavController, private alertCtrl: AlertController) {
+    }
+
+    loadData(){
+        this.taskService.all().subscribe(
+            res => {
+                this.data=res;
+                this.getDataForView();
+            }
+        );
+    }
+
+    ionViewDidEnter() {
+        this.loadData();
+    }
+
+    private getDataForView() {
+        this.items = [];
+        for (var i = 0; i < this.data.length; i++) {
+            this.items.push(JSON.stringify(this.data[i]));
+        }
+    }
+ 
+    addItem() {
+        this.nav.push(AddPage);
+    }
+
+    delete(item: string) {
+        // console.log(JSON.parse(index).Id);
+        this.taskService.delete(JSON.parse(item).Id);
+        // this.items.splice(index, 1);
+        // localStorage.setItem("todos", JSON.stringify(this.items));
+    }
+
+    update(item: string) {
+        // console.log(JSON.parse(index).Id);
+        this.nav.push(EditPage, {
+            param1: JSON.parse(item)
+        })
+        // this.items.splice(index, 1);
+        // localStorage.setItem("todos", JSON.stringify(this.items));
+    }
 
 
-
-
-  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public http: Http) {
-	  this.http.get('/api/values').map(res => res.json()).subscribe(data => {
-      this.items = data;
-	});
-	
-    
-  }
-
-ionViewDidLoad(){
 
 }
 
-  addItem() {
-      let addModal = this.modalCtrl.create(AddItemPage);
- 
-    addModal.onDidDismiss((item) => {
- 
-          if(item){
-            this.saveItem(item);
-          }
- 
-    });
- 
-    addModal.present();
- 
-  }
- 
-  saveItem(item){
-    this.items.push(item);
-  }
- 
-viewItem(item){
-  this.navCtrl.push(ItemDetailPage, {
-    item: item
-  });
-}
-
-  delete(item) {
-    console.log("delete item " + item.title);
-    this.items.splice(this.items.indexOf(item), 1);
-  }
-}
 
 
